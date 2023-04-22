27c09d5c72f9d16fe7ab9521b18bb75ca7f35923
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-04-22 22:29:23 +0700

    Update MainActivity.java

--- app/src/main/java/myid/shizuka/mergepdf/MainActivity.java
+++ app/src/main/java/myid/shizuka/mergepdf/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.ClipData;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -20,6 +21,9 @@ import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -35,6 +39,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.content.pm.PackageManager;
+import android.widget.Toast;
+
 import androidx.core.content.ContextCompat;
 import androidx.core.app.ActivityCompat;
 
@@ -236,6 +242,43 @@ public class MainActivity extends AppCompatActivity {
 
 
         );
+
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                Log.d("permission","permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions,1);
+            }
+
+
+        }
+
+        web.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+
+                Uri parse = Uri.parse(url.trim());
+                DownloadManager.Request request = new DownloadManager.Request(parse);
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie",cookies);
+                request.addRequestHeader("User-Agent",userAgent);
+                request.setDescription("Downloading file....");
+                request.setTitle(URLUtil.guessFileName(url,contentDisposition,mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(),"Downloading File",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
 
     }
     @Override
